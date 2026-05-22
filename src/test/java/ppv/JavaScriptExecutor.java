7bab9d115363574805b56ad5aab80470e360b2e3
PranavVibhute <pranavibhute4141@gmail.com>
2026-05-22 21:33:02 +0530

    set username to textfield

--- src/test/java/ppv/JavaScriptExecutor.java
+++ src/test/java/ppv/JavaScriptExecutor.java
@@ -1,18 +1,25 @@
 package ppv;
 
+import java.time.Duration;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.edge.EdgeDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class JavaScriptExecutor {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
 
 			WebDriverManager.edgedriver().setup();
 			EdgeDriver driver = new EdgeDriver();
 			driver.manage().window().maximize();
+			
+			//implicit wait
+			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 			
 			driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
 			
@@ -24,9 +31,11 @@ public class JavaScriptExecutor {
 			String title = (String) js.executeScript(s1);
 			System.out.println(title);
 			
+			//set username
+			WebElement username = driver.findElement(By.name("username"));
+			js.executeScript("arguments[0].value='Admin';",username);
 			
-			
-			
+			Thread.sleep(4000);
 			driver.close();
 	}
 
