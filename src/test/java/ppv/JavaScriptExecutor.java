78089be10ef853903f04b3a33057f26afe13d583
PranavVibhute <pranavibhute4141@gmail.com>
2026-05-24 17:53:04 +0530

    highlighting a webelement

--- src/test/java/ppv/JavaScriptExecutor.java
+++ src/test/java/ppv/JavaScriptExecutor.java
@@ -1,46 +1,63 @@
-package ppv;
+	package ppv;
+	
+	import java.time.Duration;
+	
+	import org.openqa.selenium.By;
+	import org.openqa.selenium.JavascriptExecutor;
+	import org.openqa.selenium.WebElement;
+	import org.openqa.selenium.edge.EdgeDriver;
+	
+	import io.github.bonigarcia.wdm.WebDriverManager;
+	
+	public class JavaScriptExecutor {
+	
+		public static void main(String[] args) throws InterruptedException {
+			// TODO Auto-generated method stub
+	
+				WebDriverManager.edgedriver().setup();
+				EdgeDriver driver = new EdgeDriver();
+				driver.manage().window().maximize();
+				
+				//implicit wait
+				driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+				
+				driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+				
+				//creating javascript executors object
+				JavascriptExecutor js =  (JavascriptExecutor)driver;
+				
+				//returns webpage title
+				String s1 = "return document.title";
+				String title = (String) js.executeScript(s1);
+				System.out.println(title);
+				
+				//set username and password
+				WebElement username = driver.findElement(By.name("username"));
+				WebElement Pass = driver.findElement(By.name("password"));
+//				String s2 = "arguments[0].value='Admin';"
+//						+ "arguments[1].value='admin123';";
+//				js.executeScript(s2,username,Pass);
+				String s2 =
+					    "arguments[0].value='Admin';" +
+					    "arguments[0].dispatchEvent(new Event('input',{bubbles:true}));" +
+					    "arguments[1].value='admin123';" +
+					    "arguments[1].dispatchEvent(new Event('input',{bubbles:true}));";
 
-import java.time.Duration;
-
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.edge.EdgeDriver;
-
-import io.github.bonigarcia.wdm.WebDriverManager;
-
-public class JavaScriptExecutor {
-
-	public static void main(String[] args) throws InterruptedException {
-		// TODO Auto-generated method stub
-
-			WebDriverManager.edgedriver().setup();
-			EdgeDriver driver = new EdgeDriver();
-			driver.manage().window().maximize();
-			
-			//implicit wait
-			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-			
-			driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
-			
-			//creating javascript executors object
-			JavascriptExecutor js =  (JavascriptExecutor)driver;
-			
-			//returns webpage title
-			String s1 = "return document.title";
-			String title = (String) js.executeScript(s1);
-			System.out.println(title);
-			
-			//set username and password
-			WebElement username = driver.findElement(By.name("username"));
-			WebElement Pass = driver.findElement(By.name("password"));
-			String s2 = "arguments[0].value='Admin';"
-					+ "arguments[1].value='admin123';";
-			js.executeScript(s2,username,Pass);
-			
-			
-			Thread.sleep(4000);
-			driver.close();
+					js.executeScript(s2, username, Pass);
+				
+				System.out.println(username.getAttribute("value"));
+				System.out.println(Pass.getAttribute("value"));
+				
+				//highlighting a webelement
+				WebElement btn = driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div/div[1]/div/div[2]/div[2]/form/div[3]/button"));
+				
+				String s3 = "arguments[0].style.backgroundColor = 'red';"+
+							"arguments[0].click();";
+				js.executeScript(s3, btn);
+								
+				
+				Thread.sleep(4000);
+				driver.close();
+		}
+	
 	}
-
-}
