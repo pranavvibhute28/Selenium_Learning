d82424781a7a4c9632f48baf985e82ecfff1b019
PranavVibhute <pranavibhute4141@gmail.com>
2026-05-27 18:03:00 +0530

    added priority

--- src/test/java/ppv/TestNgDemo.java
+++ src/test/java/ppv/TestNgDemo.java
@@ -4,9 +4,14 @@ import org.testng.annotations.Test;
 
 public class TestNgDemo {
 	
-	@Test
-	public void fucntion2() {
+	@Test(priority = 1, description="this is function 2 description")
+	public void xyz() {
 		System.out.println("This is fucnction 2 ");
+	}
+	
+	@Test(priority = 2, description="this is function 1 description")
+	public void abc() {
+		System.out.println("This is fucnction 1 ");
 	}
 
 }
